c56cdd668bffa5ef4e4b087fbc45ee0a32b5c781
Suazoo <89082251a@gmail.com>
2026-04-26 20:36:08 -0600

    permiso figma

--- Program.cs
+++ Program.cs
@@ -44,9 +44,13 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
     {
-        policy.WithOrigins("http://localhost:3000", "http://localhost:5173")
+        policy.WithOrigins("http://localhost:3000", 
+                "http://localhost:5173",
+                "https://www.figma.com",
+                "https://figma.com")
               .AllowAnyHeader()
-              .AllowAnyMethod();
+              .AllowAnyMethod()
+              .AllowCredentials();
     });
 });
 
